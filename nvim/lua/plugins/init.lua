960f5cc957f8dd20c7a1f286e46c0b04be4e8f57
Farid <faridfarajli86@gmail.com>
2025-07-04 14:38:20 +0400

    nvim: add 2 new plugins
    Introduced plugins are `nvim-treesitter` and `fzf-lua`

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -15,4 +15,27 @@ return {
 		cmd = "Neogit",
 		opts = {},
 	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = function()
+			local treesitter = require("nvim-treesitter")
+
+			treesitter.setup()
+			treesitter.update()
+			treesitter.install({ "c", "go", "make", "python", "toml" }):wait(600000)
+		end,
+		branch = "main",
+		config = function()
+			vim.api.nvim_create_autocmd('FileType', {
+				pattern = { "c", "go", "make", "python", "toml" },
+				callback = function()
+					vim.treesitter.start()
+				end,
+			})
+		end,
+	},
+	{
+		"ibhagwan/fzf-lua",
+		opts = {},
+	},
 }
