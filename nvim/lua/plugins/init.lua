9dfa9a88200b30747ac9e9d7988445397578bf9b
Farid <faridfarajli86@gmail.com>
2025-07-11 14:36:56 +0400

    nvim: add c++ parsers to treesitter

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -22,12 +22,12 @@ return {
 
 			treesitter.setup()
 			treesitter.update()
-			treesitter.install({ "c", "go", "make", "python", "toml" }):wait(600000)
+			treesitter.install({ "c", "go", "make", "python", "toml", "cpp" }):wait(600000)
 		end,
 		branch = "main",
 		config = function()
 			vim.api.nvim_create_autocmd('FileType', {
-				pattern = { "c", "go", "make", "python", "toml" },
+				pattern = { "c", "go", "make", "python", "toml", "cpp" },
 				callback = function()
 					vim.treesitter.start()
 				end,
