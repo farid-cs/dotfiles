0cb48b4c48820d8b4bd13bd807a7f2b18efd6045
Farid <faridfarajli86@gmail.com>
2025-08-26 23:16:22 +0400

    nvim: add ini to treesitter parsers

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -22,12 +22,12 @@ return {
 
 			treesitter.setup()
 			treesitter.update()
-			treesitter.install({ "c", "go", "make", "python", "toml", "cpp" }):wait(600000)
+			treesitter.install({ "c", "go", "make", "python", "toml", "cpp", "ini" }):wait(600000)
 		end,
 		branch = "main",
 		config = function()
 			vim.api.nvim_create_autocmd('FileType', {
-				pattern = { "c", "go", "make", "python", "toml", "cpp" },
+				pattern = { "c",  "dosini", "go", "make", "python", "toml", "cpp" },
 				callback = function()
 					vim.treesitter.start()
 				end,
