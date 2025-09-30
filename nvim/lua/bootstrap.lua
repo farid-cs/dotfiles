b7b6bb2a573221194f28ff2d5ac266b9274c3d46
Farid <faridfarajli86@gmail.com>
2025-09-30 21:07:19 +0400

    Revert "nvim: install lazy.nvim using built-in plugin manager"
    [Why]
    Built-in plugin manager is a new feature of neovim 0.12 which is not
    released yet.
    
    This reverts commit 69fdbc4163685b8c7b82627ad435ca2a00b9a839.

--- nvim/lua/bootstrap.lua
+++ nvim/lua/bootstrap.lua
@@ -1,2 +1,10 @@
-vim.pack.add({ 'https://github.com/folke/lazy.nvim' })
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+	local repo = "https://github.com/folke/lazy.nvim.git"
+	vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", repo, lazypath })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
 require("lazy").setup({ import = "plugins" })
